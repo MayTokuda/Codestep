5d2f3be4faff6082f8689312e5c209dd5e86dfdb
MayTokuda <mei1984.05.18@gmail.com>
2022-10-16 01:46:08 +0900

    PC表示完成

--- 15_BBB英会話スクール/main.js
+++ 15_BBB英会話スクール/main.js
@@ -12,6 +12,31 @@ $(function() {
             $('.globalMenuSp').removeClass('active');
         }
     });
+
+    //================================================
+    //  スライドイン
+    //================================================
+    $('.left').each(function(){
+        let $item = $(this);
+        $(window).on('scroll', function(){
+          let top = $item.offset().top; // ターゲットの位置取得
+          let position = top - $(window).height();  // イベントを発火させたい位置
+          if($(window).scrollTop() > position){
+            $item.addClass('slide-left');
+          }
+        });
+    });
+
+    $('.right').each(function(){
+        let $item = $(this);
+        $(window).on('scroll', function(){
+          let top = $item.offset().top; // ターゲットの位置取得
+          let position = top - $(window).height();  // イベントを発火させたい位置
+          if($(window).scrollTop() > position){
+            $item.addClass('slide-right');
+          }
+        });
+    });
 });
 
 
